c1d9ac4f64ed8c04566a99fe1d5d678d29dc0e19
YoursFunny <admin@yoursfunny.top>
2023-11-16 20:49:11 +0800

    feat: add assets for scrimmage (school exchange)

---
+++ tasks/school_exchange/assets/assets_school_exchange.py
@@ -0,0 +1,71 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+CHECK_GEHENNA = ButtonWrapper(
+    name='CHECK_GEHENNA',
+    jp=Button(
+        file='./assets/jp/school_exchange/CHECK_GEHENNA.png',
+        area=(109, 149, 195, 177),
+        search=(89, 129, 215, 197),
+        color=(201, 205, 206),
+        button=(109, 149, 195, 177),
+    ),
+    en=None,
+)
+CHECK_MILLENNIUM = ButtonWrapper(
+    name='CHECK_MILLENNIUM',
+    jp=Button(
+        file='./assets/jp/school_exchange/CHECK_MILLENNIUM.png',
+        area=(108, 148, 254, 178),
+        search=(88, 128, 274, 198),
+        color=(202, 206, 208),
+        button=(108, 148, 254, 178),
+    ),
+    en=None,
+)
+CHECK_TRINITY = ButtonWrapper(
+    name='CHECK_TRINITY',
+    jp=Button(
+        file='./assets/jp/school_exchange/CHECK_TRINITY.png',
+        area=(115, 148, 250, 178),
+        search=(95, 128, 270, 198),
+        color=(204, 208, 210),
+        button=(115, 148, 250, 178),
+    ),
+    en=None,
+)
+SELECT_GEHENNA = ButtonWrapper(
+    name='SELECT_GEHENNA',
+    jp=Button(
+        file='./assets/jp/school_exchange/SELECT_GEHENNA.png',
+        area=(1125, 275, 1224, 310),
+        search=(1105, 255, 1244, 330),
+        color=(207, 217, 225),
+        button=(1125, 275, 1224, 310),
+    ),
+    en=None,
+)
+SELECT_MILLENNIUM = ButtonWrapper(
+    name='SELECT_MILLENNIUM',
+    jp=Button(
+        file='./assets/jp/school_exchange/SELECT_MILLENNIUM.png',
+        area=(1069, 381, 1217, 417),
+        search=(1049, 361, 1237, 437),
+        color=(206, 214, 222),
+        button=(1069, 381, 1217, 417),
+    ),
+    en=None,
+)
+SELECT_TRINITY = ButtonWrapper(
+    name='SELECT_TRINITY',
+    jp=Button(
+        file='./assets/jp/school_exchange/SELECT_TRINITY.png',
+        area=(1074, 165, 1221, 204),
+        search=(1054, 145, 1241, 224),
+        color=(213, 221, 228),
+        button=(1074, 165, 1221, 204),
+    ),
+    en=None,
+)
