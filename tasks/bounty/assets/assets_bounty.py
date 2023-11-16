f506616ba93a52c6abc804ba97bdb4b5f7207d07
YoursFunny <admin@yoursfunny.top>
2023-11-16 20:49:26 +0800

    feat: add assets for bounty

---
+++ tasks/bounty/assets/assets_bounty.py
@@ -0,0 +1,71 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+CHECK_DESERT_RAILROAD = ButtonWrapper(
+    name='CHECK_DESERT_RAILROAD',
+    jp=Button(
+        file='./assets/jp/bounty/CHECK_DESERT_RAILROAD.png',
+        area=(106, 147, 401, 179),
+        search=(86, 127, 421, 199),
+        color=(172, 179, 183),
+        button=(106, 147, 401, 179),
+    ),
+    en=None,
+)
+CHECK_HIGHWAY = ButtonWrapper(
+    name='CHECK_HIGHWAY',
+    jp=Button(
+        file='./assets/jp/bounty/CHECK_HIGHWAY.png',
+        area=(107, 147, 400, 179),
+        search=(87, 127, 420, 199),
+        color=(191, 199, 203),
+        button=(107, 147, 400, 179),
+    ),
+    en=None,
+)
+CHECK_SCHOOLHOUSE = ButtonWrapper(
+    name='CHECK_SCHOOLHOUSE',
+    jp=Button(
+        file='./assets/jp/bounty/CHECK_SCHOOLHOUSE.png',
+        area=(106, 147, 314, 179),
+        search=(86, 127, 334, 199),
+        color=(176, 183, 187),
+        button=(106, 147, 314, 179),
+    ),
+    en=None,
+)
+SELECT_DESERT_RAILROAD = ButtonWrapper(
+    name='SELECT_DESERT_RAILROAD',
+    jp=Button(
+        file='./assets/jp/bounty/SELECT_DESERT_RAILROAD.png',
+        area=(1066, 271, 1224, 311),
+        search=(1046, 251, 1244, 331),
+        color=(178, 188, 199),
+        button=(1066, 271, 1224, 311),
+    ),
+    en=None,
+)
+SELECT_HIGHWAY = ButtonWrapper(
+    name='SELECT_HIGHWAY',
+    jp=Button(
+        file='./assets/jp/bounty/SELECT_HIGHWAY.png',
+        area=(1065, 165, 1223, 203),
+        search=(1045, 145, 1243, 223),
+        color=(214, 221, 228),
+        button=(1065, 165, 1223, 203),
+    ),
+    en=None,
+)
+SELECT_SCHOOLHOUSE = ButtonWrapper(
+    name='SELECT_SCHOOLHOUSE',
+    jp=Button(
+        file='./assets/jp/bounty/SELECT_SCHOOLHOUSE.png',
+        area=(1154, 381, 1223, 417),
+        search=(1134, 361, 1243, 437),
+        color=(173, 185, 198),
+        button=(1154, 381, 1223, 417),
+    ),
+    en=None,
+)
