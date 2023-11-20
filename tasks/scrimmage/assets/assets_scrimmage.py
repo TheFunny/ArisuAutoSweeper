d3a1a77d6aa174dfe9c62869620581fbd741630b
YoursFunny <admin@yoursfunny.top>
2023-11-20 21:28:34 +0800

    feat: add scrimmage assets

--- tasks/scrimmage/assets/assets_scrimmage.py
+++ tasks/scrimmage/assets/assets_scrimmage.py
@@ -6,7 +6,7 @@ from module.base.button import Button, ButtonWrapper
 CHECK_GEHENNA = ButtonWrapper(
     name='CHECK_GEHENNA',
     jp=Button(
-        file='./assets/jp/school_exchange/CHECK_GEHENNA.png',
+        file='./assets/jp/scrimmage/CHECK_GEHENNA.png',
         area=(109, 149, 195, 177),
         search=(89, 129, 215, 197),
         color=(201, 205, 206),
@@ -17,7 +17,7 @@ CHECK_GEHENNA = ButtonWrapper(
 CHECK_MILLENNIUM = ButtonWrapper(
     name='CHECK_MILLENNIUM',
     jp=Button(
-        file='./assets/jp/school_exchange/CHECK_MILLENNIUM.png',
+        file='./assets/jp/scrimmage/CHECK_MILLENNIUM.png',
         area=(108, 148, 254, 178),
         search=(88, 128, 274, 198),
         color=(202, 206, 208),
@@ -25,10 +25,21 @@ CHECK_MILLENNIUM = ButtonWrapper(
     ),
     en=None,
 )
+CHECK_SCRIMMAGE = ButtonWrapper(
+    name='CHECK_SCRIMMAGE',
+    jp=Button(
+        file='./assets/jp/scrimmage/CHECK_SCRIMMAGE.png',
+        area=(651, 90, 889, 130),
+        search=(631, 70, 909, 150),
+        color=(96, 119, 144),
+        button=(651, 90, 889, 130),
+    ),
+    en=None,
+)
 CHECK_TRINITY = ButtonWrapper(
     name='CHECK_TRINITY',
     jp=Button(
-        file='./assets/jp/school_exchange/CHECK_TRINITY.png',
+        file='./assets/jp/scrimmage/CHECK_TRINITY.png',
         area=(115, 148, 250, 178),
         search=(95, 128, 270, 198),
         color=(204, 208, 210),
@@ -36,10 +47,21 @@ CHECK_TRINITY = ButtonWrapper(
     ),
     en=None,
 )
+OCR_TICKET = ButtonWrapper(
+    name='OCR_TICKET',
+    jp=Button(
+        file='./assets/jp/scrimmage/OCR_TICKET.png',
+        area=(195, 85, 235, 113),
+        search=(175, 65, 255, 133),
+        color=(206, 211, 215),
+        button=(195, 85, 235, 113),
+    ),
+    en=None,
+)
 SELECT_GEHENNA = ButtonWrapper(
     name='SELECT_GEHENNA',
     jp=Button(
-        file='./assets/jp/school_exchange/SELECT_GEHENNA.png',
+        file='./assets/jp/scrimmage/SELECT_GEHENNA.png',
         area=(1125, 275, 1224, 310),
         search=(1105, 255, 1244, 330),
         color=(207, 217, 225),
@@ -50,7 +72,7 @@ SELECT_GEHENNA = ButtonWrapper(
 SELECT_MILLENNIUM = ButtonWrapper(
     name='SELECT_MILLENNIUM',
     jp=Button(
-        file='./assets/jp/school_exchange/SELECT_MILLENNIUM.png',
+        file='./assets/jp/scrimmage/SELECT_MILLENNIUM.png',
         area=(1069, 381, 1217, 417),
         search=(1049, 361, 1237, 437),
         color=(206, 214, 222),
@@ -61,7 +83,7 @@ SELECT_MILLENNIUM = ButtonWrapper(
 SELECT_TRINITY = ButtonWrapper(
     name='SELECT_TRINITY',
     jp=Button(
-        file='./assets/jp/school_exchange/SELECT_TRINITY.png',
+        file='./assets/jp/scrimmage/SELECT_TRINITY.png',
         area=(1074, 165, 1221, 204),
         search=(1054, 145, 1241, 224),
         color=(213, 221, 228),
