17964cbd9a7857e72dae2a4bb27e70cc5668cfff
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2023-12-22 05:35:33 +0000

    Added Tasks, Shop, MomoTalk (#11)
    * feat: tasks
    
    Added module tasks for EN
    
    * refactor: gui
    
    added tree view Farm and Reward.
    
    * feat: shop
    
    * feat: momotalk
    
    ---------
    
    Co-authored-by: YoursFunny <admin@yoursfunny.top>

---
+++ tasks/shop/assets/assets_shop.py
@@ -0,0 +1,115 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+CONFIRM_PURCHASE = ButtonWrapper(
+    name='CONFIRM_PURCHASE',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/CONFIRM_PURCHASE.png',
+        area=(467, 231, 807, 309),
+        search=(447, 211, 827, 329),
+        color=(217, 218, 219),
+        button=(668, 458, 865, 514),
+    ),
+)
+CONFIRM_REFRESH = ButtonWrapper(
+    name='CONFIRM_REFRESH',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/CONFIRM_REFRESH.png',
+        area=(474, 271, 806, 306),
+        search=(454, 251, 826, 326),
+        color=(202, 203, 204),
+        button=(675, 434, 863, 500),
+    ),
+)
+ITEM_LIST = ButtonWrapper(
+    name='ITEM_LIST',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/ITEM_LIST.png',
+        area=(625, 127, 1244, 610),
+        search=(605, 107, 1264, 630),
+        color=(193, 206, 213),
+        button=(625, 127, 1244, 610),
+    ),
+)
+NORMAL_OFF = ButtonWrapper(
+    name='NORMAL_OFF',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/NORMAL_OFF.png',
+        area=(4, 111, 213, 167),
+        search=(0, 91, 233, 187),
+        color=(248, 248, 245),
+        button=(4, 111, 213, 167),
+    ),
+)
+NORMAL_ON = ButtonWrapper(
+    name='NORMAL_ON',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/NORMAL_ON.png',
+        area=(4, 109, 212, 170),
+        search=(0, 89, 232, 190),
+        color=(57, 78, 96),
+        button=(4, 109, 212, 170),
+    ),
+)
+OCR_REFRESH = ButtonWrapper(
+    name='OCR_REFRESH',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/OCR_REFRESH.png',
+        area=(712, 302, 762, 344),
+        search=(692, 282, 782, 364),
+        color=(225, 225, 226),
+        button=(712, 302, 762, 344),
+    ),
+)
+PURCHASE = ButtonWrapper(
+    name='PURCHASE',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/PURCHASE.png',
+        area=(1102, 640, 1227, 684),
+        search=(1082, 620, 1247, 704),
+        color=(226, 206, 65),
+        button=(1102, 640, 1227, 684),
+    ),
+)
+REFRESH = ButtonWrapper(
+    name='REFRESH',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/REFRESH.png',
+        area=(1098, 643, 1223, 682),
+        search=(1078, 623, 1243, 702),
+        color=(231, 234, 237),
+        button=(1098, 643, 1223, 682),
+    ),
+)
+TC_OFF = ButtonWrapper(
+    name='TC_OFF',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/TC_OFF.png',
+        area=(2, 503, 209, 558),
+        search=(0, 483, 229, 578),
+        color=(239, 242, 244),
+        button=(2, 503, 209, 558),
+    ),
+)
+TC_ON = ButtonWrapper(
+    name='TC_ON',
+    jp=None,
+    en=Button(
+        file='./assets/en/shop/TC_ON.png',
+        area=(3, 493, 208, 548),
+        search=(0, 473, 228, 568),
+        color=(62, 84, 99),
+        button=(3, 493, 208, 548),
+    ),
+)
---
+++ tasks/shop/shop.py
@@ -0,0 +1,118 @@
+from enum import Flag
+
+from module.base.timer import Timer
+from module.exception import RequestHumanTakeover
+from module.logger import logger
+from module.ui.switch import Switch
+from tasks.base.assets.assets_base_page import BACK
+from tasks.base.page import page_main, page_shop
+from tasks.shop.assets.assets_shop import *
+from tasks.shop.ui import ShopUI
+
+
+class ShopStatus(Flag):
+    SELECT_SHOP = 0
+    SELECT_ITEMS = 1
+    PURCHASE = 2
+    REFRESH = 3
+    END = 4
+    FINISH = -1
+
+class Shop(ShopUI):
+    @property
+    def shop_info(self):
+        """Similiar to bounty_info and scrimmage_info.
+        Returns a list with elements the select button, check button, how many times do make purchases and the list of items"""
+        info = []
+        if self.config.NormalShop_Enable:
+            normal_config = self.config.cross_get(["Shop", "NormalShop"])
+            normal_items = [num for num in range(1, 21) if normal_config[str(num)]]
+            if normal_items:
+                SWITCH_NORMAL = Switch('NormalShop_Switch')
+                SWITCH_NORMAL.add_state('on', NORMAL_ON)
+                SWITCH_NORMAL.add_state('off', NORMAL_OFF)
+                info.append([SWITCH_NORMAL, self.config.NormalShop_Purchases, normal_items])
+
+        if self.config.TacticalChallengeShop_Enable:
+            tc_config = self.config.cross_get(["Shop", "TacticalChallengeShop"])
+            tc_items = [num for num in range(1, 16) if tc_config[str(num)]]
+            if tc_items:
+                SWITCH_TC = Switch('TacticalChallengeShop_Switch')
+                SWITCH_TC.add_state('on', TC_ON)
+                SWITCH_TC.add_state('off', TC_OFF)
+                info.append([SWITCH_TC, self.config.TacticalChallengeShop_Purchases, tc_items])
+
+        return info
+
+    @property
+    def valid_task(self) -> list:
+        task = self.shop_info
+        if not task:
+            logger.warning('Shop enabled but no task set')
+        return task
+
+    @property
+    def current_shop(self):
+        return self.task[0][0]
+
+    @property
+    def current_purchase_count(self):
+        return self.task[0][1]
+    
+    @property
+    def current_item_list(self):
+        return self.task[0][2]
+
+    def handle_shop(self, status):
+        match status:
+            case ShopStatus.SELECT_SHOP:
+                if not self.task:
+                    return ShopStatus.FINISH
+                if self.select_shop(self.current_shop):
+                    self.reset_swipe_flags()
+                    return ShopStatus.SELECT_ITEMS
+            case ShopStatus.SELECT_ITEMS:
+                self.select_items(self.current_item_list)
+                return ShopStatus.PURCHASE
+            case ShopStatus.PURCHASE:
+                if self.make_purchase():
+                    return ShopStatus.REFRESH
+                return ShopStatus.END
+            case ShopStatus.REFRESH:
+                if self.refresh_shop(self.current_purchase_count):
+                    return ShopStatus.SELECT_SHOP
+                return ShopStatus.END
+            case ShopStatus.END:
+                if self.appear(page_shop.check_button):
+                    self.task.pop(0)
+                    return ShopStatus.SELECT_SHOP
+                self.click_with_interval(BACK, interval=2)
+            case ShopStatus.FINISH:
+                return status
+            case _:
+                logger.warning(f'Invalid status: {status}')
+        return status
+
+    def run(self):
+        """Reset the shop and items position by going main and then shop"""
+        self.ui_ensure(page_main)
+        self.ui_ensure(page_shop)
+
+        self.task = self.valid_task
+        action_timer = Timer(0.5, 1)
+        status = ShopStatus.SELECT_SHOP
+
+        while 1:
+            self.device.screenshot()
+
+            if self.ui_additional():
+                continue
+
+            if action_timer.reached_and_reset():
+                logger.attr('Status', status)
+                status = self.handle_shop(status)
+
+            if status == ShopStatus.FINISH:
+                break
+
+        self.config.task_delay(server_update=True)
