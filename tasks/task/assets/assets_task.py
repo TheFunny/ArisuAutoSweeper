be3fdb09884b22e81c5dfc015e9ab637a3d9f178
YoursFunny <admin@yoursfunny.top>
2023-12-23 16:21:46 +0800

    fix(task): check all claimed

--- tasks/task/assets/assets_task.py
+++ tasks/task/assets/assets_task.py
@@ -14,6 +14,28 @@ CLAIM = ButtonWrapper(
         button=(935, 639, 1015, 698),
     ),
 )
+CLAIMED = ButtonWrapper(
+    name='CLAIMED',
+    jp=None,
+    en=Button(
+        file='./assets/en/task/CLAIMED.png',
+        area=(937, 641, 1010, 696),
+        search=(917, 621, 1030, 716),
+        color=(212, 212, 211),
+        button=(937, 641, 1010, 696),
+    ),
+)
+CLAIMED_ALL = ButtonWrapper(
+    name='CLAIMED_ALL',
+    jp=None,
+    en=Button(
+        file='./assets/en/task/CLAIMED_ALL.png',
+        area=(1058, 641, 1240, 701),
+        search=(1038, 621, 1260, 720),
+        color=(193, 193, 196),
+        button=(1058, 641, 1240, 701),
+    ),
+)
 CLAIM_ALL = ButtonWrapper(
     name='CLAIM_ALL',
     jp=None,
