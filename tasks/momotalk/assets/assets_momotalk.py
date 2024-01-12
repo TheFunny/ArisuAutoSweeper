8287822fe415b4474a68205dda8e49e33cf8ab70
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2024-01-12 18:55:59 +0000

    feat: momotalk zht

--- tasks/momotalk/assets/assets_momotalk.py
+++ tasks/momotalk/assets/assets_momotalk.py
@@ -15,10 +15,10 @@ BEGIN_STORY = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/BEGIN_STORY.png',
-        area=(796, 540, 1059, 591),
-        search=(776, 520, 1079, 611),
-        color=(107, 197, 230),
-        button=(796, 540, 1059, 591),
+        area=(729, 533, 1114, 599),
+        search=(709, 513, 1134, 619),
+        color=(110, 205, 239),
+        button=(729, 533, 1114, 599),
     ),
 )
 CHATTING = ButtonWrapper(
@@ -69,10 +69,10 @@ CONFIRM_SKIP = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/CONFIRM_SKIP.png',
-        area=(674, 486, 871, 555),
-        search=(654, 466, 891, 575),
-        color=(112, 207, 241),
-        button=(674, 486, 871, 555),
+        area=(665, 492, 875, 555),
+        search=(645, 472, 895, 575),
+        color=(111, 209, 244),
+        button=(665, 492, 875, 555),
     ),
 )
 CONFIRM_SORT = ButtonWrapper(
@@ -87,10 +87,10 @@ CONFIRM_SORT = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/CONFIRM_SORT.png',
-        area=(239, 407, 657, 439),
-        search=(219, 387, 677, 459),
-        color=(248, 249, 249),
-        button=(239, 407, 657, 439),
+        area=(240, 401, 656, 440),
+        search=(220, 381, 676, 460),
+        color=(249, 250, 250),
+        button=(240, 401, 656, 440),
     ),
 )
 FIRST_UNREAD = ButtonWrapper(
@@ -123,10 +123,10 @@ MENU = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/MENU.png',
-        area=(1156, 15, 1251, 63),
-        search=(1136, 0, 1271, 83),
-        color=(215, 219, 222),
-        button=(1156, 15, 1251, 63),
+        area=(1151, 20, 1256, 61),
+        search=(1131, 0, 1276, 81),
+        color=(214, 218, 223),
+        button=(1151, 20, 1256, 61),
     ),
 )
 NOTIFICATION_BADGE = ButtonWrapper(
@@ -159,10 +159,10 @@ REPLY = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/REPLY.png',
-        area=(791, 431, 855, 462),
-        search=(771, 411, 875, 482),
-        color=(198, 210, 216),
-        button=(791, 431, 855, 462),
+        area=(783, 428, 838, 460),
+        search=(763, 408, 858, 480),
+        color=(189, 203, 211),
+        button=(783, 428, 838, 460),
     ),
 )
 SELECT_STUDENT = ButtonWrapper(
@@ -177,10 +177,10 @@ SELECT_STUDENT = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/SELECT_STUDENT.png',
-        area=(839, 369, 998, 403),
-        search=(819, 349, 1018, 423),
-        color=(241, 242, 242),
-        button=(839, 369, 998, 403),
+        area=(849, 365, 980, 406),
+        search=(829, 345, 1000, 426),
+        color=(236, 237, 238),
+        button=(849, 365, 980, 406),
     ),
 )
 SKIP = ButtonWrapper(
@@ -195,10 +195,10 @@ SKIP = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/SKIP.png',
-        area=(1192, 103, 1229, 141),
-        search=(1172, 83, 1249, 161),
-        color=(108, 125, 145),
-        button=(1192, 103, 1229, 141),
+        area=(1186, 95, 1231, 144),
+        search=(1166, 75, 1251, 164),
+        color=(85, 105, 129),
+        button=(1186, 95, 1231, 144),
     ),
 )
 SORT_ASCENDING = ButtonWrapper(
@@ -249,10 +249,10 @@ STORY = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/STORY.png',
-        area=(790, 529, 979, 557),
-        search=(770, 509, 999, 577),
-        color=(220, 208, 214),
-        button=(790, 529, 979, 557),
+        area=(781, 527, 878, 555),
+        search=(761, 507, 898, 575),
+        color=(208, 197, 204),
+        button=(781, 527, 878, 555),
     ),
 )
 SWITCH_MESSAGE = ButtonWrapper(
@@ -285,10 +285,10 @@ SWITCH_MESSAGE_CHECK = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/SWITCH_MESSAGE_CHECK.png',
-        area=(228, 165, 391, 194),
-        search=(208, 145, 411, 214),
-        color=(211, 215, 217),
-        button=(228, 165, 391, 194),
+        area=(226, 161, 320, 194),
+        search=(206, 141, 340, 214),
+        color=(199, 203, 205),
+        button=(226, 161, 320, 194),
     ),
 )
 SWITCH_STUDENT = ButtonWrapper(
@@ -321,10 +321,10 @@ SWITCH_STUDENT_CHECK = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/SWITCH_STUDENT_CHECK.png',
-        area=(229, 166, 320, 192),
-        search=(209, 146, 340, 212),
-        color=(199, 203, 205),
-        button=(229, 166, 320, 192),
+        area=(221, 161, 281, 197),
+        search=(201, 141, 301, 217),
+        color=(210, 214, 216),
+        button=(221, 161, 281, 197),
     ),
 )
 UNREAD = ButtonWrapper(
@@ -339,10 +339,10 @@ UNREAD = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/UNREAD.png',
-        area=(454, 160, 568, 193),
-        search=(434, 140, 588, 213),
+        area=(453, 160, 567, 195),
+        search=(433, 140, 587, 215),
         color=(241, 242, 243),
-        button=(454, 160, 568, 193),
+        button=(453, 160, 567, 195),
     ),
 )
 UNREAD_OFF = ButtonWrapper(
@@ -357,10 +357,10 @@ UNREAD_OFF = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/UNREAD_OFF.png',
-        area=(456, 273, 658, 316),
-        search=(436, 253, 678, 336),
-        color=(252, 252, 251),
-        button=(456, 273, 658, 316),
+        area=(459, 274, 658, 314),
+        search=(439, 254, 678, 334),
+        color=(251, 251, 251),
+        button=(459, 274, 658, 314),
     ),
 )
 UNREAD_ON = ButtonWrapper(
@@ -375,9 +375,9 @@ UNREAD_ON = ButtonWrapper(
     ),
     zht=Button(
         file='./assets/zht/momotalk/UNREAD_ON.png',
-        area=(456, 272, 658, 314),
-        search=(436, 252, 678, 334),
-        color=(245, 120, 144),
-        button=(456, 272, 658, 314),
+        area=(455, 272, 658, 314),
+        search=(435, 252, 678, 334),
+        color=(244, 118, 143),
+        button=(455, 272, 658, 314),
     ),
 )
