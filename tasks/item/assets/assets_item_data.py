1d618d1624ddf425e926d0e440f50a2a4ab39edb
YoursFunny <admin@yoursfunny.top>
2025-08-09 13:19:41 +0800

    fix: update assets file

--- tasks/item/assets/assets_item_data.py
+++ tasks/item/assets/assets_item_data.py
@@ -5,35 +5,21 @@ from module.base.button import Button, ButtonWrapper
 
 OCR_AP = ButtonWrapper(
     name='OCR_AP',
-    jp=Button(
-        file='./assets/jp/item/data/OCR_AP.png',
+    share=Button(
+        file='./assets/share/item/data/OCR_AP.png',
         area=(535, 13, 660, 36),
         search=(515, 0, 680, 56),
         color=(207, 215, 220),
         button=(535, 13, 660, 36),
     ),
-    share=Button(
-        file='./assets/share/item/data/OCR_AP.png',
-        area=(560, 11, 667, 37),
-        search=(540, 0, 687, 57),
-        color=(211, 216, 219),
-        button=(560, 11, 667, 37),
-    ),
 )
 OCR_DATA = ButtonWrapper(
     name='OCR_DATA',
-    jp=Button(
-        file='./assets/jp/item/data/OCR_DATA.png',
+    share=Button(
+        file='./assets/share/item/data/OCR_DATA.png',
         area=(745, 11, 1069, 39),
         search=(725, 0, 1089, 59),
         color=(216, 223, 227),
         button=(745, 11, 1069, 39),
-    ),
-    share=Button(
-        file='./assets/share/item/data/OCR_DATA.png',
-        area=(768, 12, 1072, 37),
-        search=(748, 0, 1092, 57),
-        color=(212, 220, 224),
-        button=(768, 12, 1072, 37),
     ),
 )
