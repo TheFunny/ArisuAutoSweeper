0b1a4efd09906200e7b266c8d833c9127a2a0829
YoursFunny <admin@yoursfunny.top>
2026-07-05 16:36:01 +0800

    fix: renew timer in `interval_is_reached()` only

--- module/base/base.py
+++ module/base/base.py
@@ -221,7 +221,7 @@ class ModuleBase:
         button_area = area_offset((-encourage, -encourage, encourage, encourage), offset=point)
         return ClickButton(area=button_area, name=name)
 
-    def get_interval_timer(self, button, interval=5) -> Timer:
+    def get_interval_timer(self, button, interval=5, renew=False) -> Timer:
         if hasattr(button, 'name'):
             name = button.name
         elif callable(button):
@@ -231,7 +231,7 @@ class ModuleBase:
 
         try:
             timer = self.interval_timer[name]
-            if timer.limit != interval:
+            if renew and timer.limit != interval:
                 timer = Timer(interval)
                 self.interval_timer[name] = timer
             return timer
@@ -259,7 +259,7 @@ class ModuleBase:
             self.get_interval_timer(button, interval=interval).clear()
 
     def interval_is_reached(self, button, interval=5):
-        return self.get_interval_timer(button, interval=interval).reached()
+        return self.get_interval_timer(button, interval=interval, renew=True).reached()
 
 
     _image_file = ''
