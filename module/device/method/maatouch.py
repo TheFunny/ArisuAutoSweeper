810e669118d93fbabc6798bc532f16484ade7714
YoursFunny <admin@yoursfunny.top>
2026-07-05 23:00:23 +0800

    fix: add retry for minitouch and MaaTouch builders

--- module/device/method/maatouch.py
+++ module/device/method/maatouch.py
@@ -107,6 +107,7 @@ class MaaTouch(Connection):
     _maatouch_init_thread = None
 
     @cached_property
+    @retry
     def _maatouch_builder(self):
         self.maatouch_init()
         return MaatouchBuilder(self)
@@ -298,3 +299,8 @@ class MaaTouch(Connection):
         builder.contact = 1
         builder.up().commit()
         builder.send()
+
+
+if __name__ == '__main__':
+    self = MaaTouch('src')
+    self.maatouch_uninstall()
