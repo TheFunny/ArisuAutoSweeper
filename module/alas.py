511a5b40ca004ec77e47ab7b95267c6a9e472fb6
YoursFunny <admin@yoursfunny.top>
2026-07-06 00:24:35 +0800

    feat: implement error logging functionality in new error.py module

--- module/alas.py
+++ module/alas.py
@@ -1,5 +1,5 @@
-import os
-import re
+import platform
+import subprocess
 import threading
 import time
 from datetime import datetime, timedelta
@@ -7,16 +7,14 @@ from datetime import datetime, timedelta
 import inflection
 from cached_property import cached_property
 
+from MCE.custom_widgets.ctkmessagebox import CTkMessagebox
 from module.base.decorator import del_cached_property
 from module.config.config import AzurLaneConfig, TaskEnd
 from module.config.utils import deep_get, deep_set
 from module.exception import *
-from module.logger import logger
+from module.logger import logger, save_error_log
 from module.notify import handle_notify
 
-from MCE.custom_widgets.ctkmessagebox import CTkMessagebox
-import subprocess
-import platform
 
 class AzurLaneAutoScript:
     stop_event: threading.Event = None
@@ -153,33 +151,7 @@ class AzurLaneAutoScript:
         Save last 60 screenshots in ./log/error/<timestamp>
         Save logs to ./log/error/<timestamp>/log.txt
         """
-        from module.base.utils import save_image
-        from module.handler.sensitive_info import (handle_sensitive_image, handle_sensitive_logs)
-        if self.config.Error_SaveError:
-            folder = f'./log/error/{int(time.time() * 1000)}'
-            logger.warning(f'Saving error: {folder}')
-            os.makedirs(folder, exist_ok=True)
-            for data in self.device.screenshot_deque:
-                image_time = datetime.strftime(data['time'], '%Y-%m-%d_%H-%M-%S-%f')
-                image = handle_sensitive_image(data['image'])
-                save_image(image, f'{folder}/{image_time}.png')
-            if self.device.screenshot_tracking:
-                os.makedirs(f'{folder}/tracking', exist_ok=True)
-            for data in self.device.screenshot_tracking:
-                image_time = datetime.strftime(data['time'], '%Y-%m-%d_%H-%M-%S-%f')
-                with open(f'{folder}/tracking/{image_time}.png', 'wb') as f:
-                    f.write(data['image'].getvalue())
-            with open(logger.log_file, 'r', encoding='utf-8') as f:
-                lines = f.readlines()
-                start = 0
-                for index, line in enumerate(lines):
-                    line = line.strip(' \r\t\n')
-                    if re.match('^═{15,}$', line):
-                        start = index
-                lines = lines[start - 2:]
-                lines = handle_sensitive_logs(lines)
-            with open(f'{folder}/log.txt', 'w', encoding='utf-8') as f:
-                f.writelines(lines)
+        save_error_log(config=self.config, device=self.device)
 
     def wait_until(self, future):
         """
