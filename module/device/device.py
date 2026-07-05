3d5c2ee5372b350f5284c9ff73943eccf171ccba
YoursFunny <admin@yoursfunny.top>
2026-07-06 01:45:29 +0800

    feat: add interval on dump_hierarchy()

--- module/device/device.py
+++ module/device/device.py
@@ -1,5 +1,7 @@
 import collections
 
+from lxml import etree
+
 # Patch pkg_resources before importing adbutils and uiautomator2
 from module.device.pkg_resources import get_distribution
 
@@ -155,6 +157,10 @@ class Device(Screenshot, Control, AppControl):
                 raise
 
         return self.image
+
+    def dump_hierarchy(self) -> etree._Element:
+        self.stuck_record_check()
+        return super().dump_hierarchy()
 
     def release_during_wait(self):
         # Scrcpy server is still sending video stream,
