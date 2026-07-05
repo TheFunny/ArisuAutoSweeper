6f6dab1c04b25ea0cbf0cc6c2b80580426ccc9da
YoursFunny <admin@yoursfunny.top>
2026-07-05 16:57:17 +0800

    fix: correct function to retrieve last server update

--- module/config/utils.py
+++ module/config/utils.py
@@ -539,7 +539,7 @@ def get_server_last_monday_update(daily_trigger):
     Returns:
         datetime.datetime
     """
-    update = get_server_next_update(daily_trigger)
+    update = get_server_last_update(daily_trigger)
     diff = update.weekday()
     update = update - timedelta(days=diff)
     return update
