d83ce3bdc36fd19e33423b8e7e9aef07a8bcc6ec
YoursFunny <admin@yoursfunny.top>
2026-07-06 11:32:59 +0800

    feat: add error postprocessing method and invoke on exceptions

--- module/alas.py
+++ module/alas.py
@@ -124,6 +124,7 @@ class AzurLaneAutoScript:
             return False
         except ScriptError as e:
             logger.exception(e)
+            self.error_postprocess()
             logger.critical('This is likely to be a mistake of developers, but sometimes just random issues')
             self.save_error_log()
             handle_notify(
@@ -134,6 +135,7 @@ class AzurLaneAutoScript:
             exit(1)
         except RequestHumanTakeover:
             logger.critical('Request human takeover')
+            self.error_postprocess()
             handle_notify(
                 self.config.Error_OnePushConfig,
                 title=f"AAS <{self.config_name}> crashed",
@@ -142,6 +144,7 @@ class AzurLaneAutoScript:
             exit(1)
         except Exception as e:
             logger.exception(e)
+            self.error_postprocess()
             self.save_error_log()
             handle_notify(
                 self.config.Error_OnePushConfig,
@@ -156,6 +159,12 @@ class AzurLaneAutoScript:
         Save logs to ./log/error/<timestamp>/log.txt
         """
         save_error_log(config=self.config, device=self.device)
+
+    def error_postprocess(self):
+        """
+        Do something when error occurred
+        """
+        pass
 
     def wait_until(self, future):
         """
