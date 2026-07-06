cf52a2fc2dcc429624d9e24cbf2fecc46949c21c
YoursFunny <admin@yoursfunny.top>
2026-07-06 11:26:20 +0800

    feat: save error log on ScriptError

--- module/alas.py
+++ module/alas.py
@@ -125,6 +125,7 @@ class AzurLaneAutoScript:
         except ScriptError as e:
             logger.exception(e)
             logger.critical('This is likely to be a mistake of developers, but sometimes just random issues')
+            self.save_error_log()
             handle_notify(
                 self.config.Error_OnePushConfig,
                 title=f"AAS <{self.config_name}> crashed",
